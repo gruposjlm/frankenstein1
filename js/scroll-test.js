2a300786e9ee07ad68a51c59716fbc086f85ab13
gruposjlm <150736528+gruposjlm@users.noreply.github.com>
2023-12-09 00:47:08 -0300

    Add files via upload

--- js/scroll-test.js
+++ js/scroll-test.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const targetSection = document.getElementById("scroll-to-test");
+    const targetSection = document.getElementById("geradorbotao");
 
     document.body.style.scrollBehavior = "smooth";
     document.body.style.scrollSnapType = "mandatory";
