2df5963a52af918e04231b6e024ca44316442068
gruposjlm <150736528+gruposjlm@users.noreply.github.com>
2023-12-09 00:28:59 -0300

    Add files via upload

--- css/scroll-test.js
+++ css/scroll-test.js
@@ -1,7 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function() {
     // Get the target section
-    const targetSection = document.getElementById("footer-test2");
-  
-    // Scroll slowly to the target section when the page loads
-    targetSection.scrollIntoView({ behavior: "smooth", block: "start" });
-  });
+    const targetSection = document.getElementById('footer-test2');
+
+    // Add a delay of 1000 milliseconds (1 second)
+    setTimeout(function() {
+        // Scroll slowly to the target section after the delay
+        targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, 1000);
+});
